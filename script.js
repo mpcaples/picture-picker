64ee92611ffa62e554b7a1c765abbbba3f29956b
Madeline Caples <madelinecaples@gmail.com>
2020-07-01 15:28:03 -0400

    Added getPhotos async function to retrieve random photos from Unsplash API

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// just a test 
+// HTML elements 
+const imageContainer = document.getElementById('image-container'); 
+const loader = document.getElementById('loader'); 
+
+// Unsplash API 
+const accessKey = 'LYVKBOGxzLRyn6OYr8W1wK1bZKd2VXEvqIxbMWh3n3g'
+const count = 10; 
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${count}`; 
+
+let photosArray = []; 
+
+// Fetch request 
+async function getPhotos() {
+    try {
+        const response = await fetch(apiURL); 
+        photosArray = await response.json(); 
+        console.log(photosArray); 
+    } catch (error) {
+
+    }
+
+    
+}; 
+
+getPhotos(); 
